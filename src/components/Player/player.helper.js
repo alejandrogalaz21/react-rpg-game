5a49857c0b3626ea14c021e8706b3a1c7b754f47
alex <jesus.galaz@softtek.com>
2020-09-02 22:06:49 -0500

    stay in boundaries

--- src/components/Player/player.helper.js
+++ src/components/Player/player.helper.js
@@ -1,18 +1,29 @@
-import { SPRITE_SIZE } from './../../config'
+import { SPRITE_SIZE, MAP_WIDTH, MAP_HEIGHT } from './../../config'
 
 export function getNewPosition(direction, oldPos) {
   switch (direction) {
     case 'LEFT':
-      return [oldPos[0] - SPRITE_SIZE, oldPos[1]]
+      let newPosLeft = [oldPos[0] - SPRITE_SIZE, oldPos[1]]
+      return observeBoundaries(newPosLeft, oldPos)
     case 'RIGHT':
-      return [oldPos[0] + SPRITE_SIZE, oldPos[1]]
+      let newPosRIGHT = [oldPos[0] + SPRITE_SIZE, oldPos[1]]
+      return observeBoundaries(newPosRIGHT, oldPos)
     case 'UP':
-      return [oldPos[0], oldPos[1] - SPRITE_SIZE]
+      let newPosUP = [oldPos[0], oldPos[1] - SPRITE_SIZE]
+      return observeBoundaries(newPosUP, oldPos)
     case 'DOWN':
-      return [oldPos[0], oldPos[1] + SPRITE_SIZE]
+      let newPosDOWN = [oldPos[0], oldPos[1] + SPRITE_SIZE]
+      return observeBoundaries(newPosDOWN, oldPos)
     default:
       return oldPos
   }
 }
 
-export function observeBoundaries(oldPos, newPos) {}
+export function observeBoundaries(newPos, oldPos) {
+  return newPos[0] >= 0 &&
+    newPos[0] <= MAP_WIDTH - SPRITE_SIZE &&
+    newPos[1] >= 0 &&
+    newPos[1] <= MAP_HEIGHT - SPRITE_SIZE
+    ? newPos
+    : oldPos
+}
